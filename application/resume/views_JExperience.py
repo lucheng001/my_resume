6e41db31bd254619cbb17e4dbbc4123426448c97
鲁成 <lc960127@gmail.com>
2016-11-29 10:40:17 +0800

    finish job experience

--- application/resume/views_JExperience.py
+++ application/resume/views_JExperience.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, abort
 from flask_login import login_required, current_user
+from playhouse.flask_utils import get_object_or_404
 from ..models import User, JobExperience
 from .forms_JExperience import addJobExperienceForm, addJobExperienceDataForm
 from . import bpResume
@@ -32,3 +33,12 @@ def addJExperience():
         JobExperience.create(**jExperienceDic)
         return redirect(url_for('bpShow.resume'))
     return render_template('show/resume/addJobExperience.html', form=form)
+
+@bpResume.route('/deleteJExperience/<int:jExperienceId>', methods=['GET', 'POST'])
+@login_required
+def deleteJExperience(jExperienceId):
+    jobExperience = get_object_or_404(JobExperience, JobExperience.id == jExperienceId)
+    if not current_user.id == jobExperience.user.id:
+        abort(403)
+    jobExperience.delete_instance()
+    return redirect(url_for('bpShow.resume'))
